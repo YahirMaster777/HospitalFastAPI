7bdc281d8180402de246691d7d6fad5c3b2d4330
YahirMaster777 <yahirperez141002@gmail.com>
2024-07-30 15:26:24 -0600

    Cambios en la api en configuracion

--- app.py
+++ app.py
@@ -5,11 +5,26 @@ from routes.user import user
 from routes.person import person
 from routes.rol import rol
 from routes.userrol import userrol
+from fastapi.middleware.cors import CORSMiddleware
 
 app=FastAPI(
     title="Privilege Care S.A. de C.V.",
     description="API para un Hospital"
 )
+
+origins = [
+    "http://localhost:8080",
+    "http://127.0.0.1:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(user)
 app.include_router(person)
 app.include_router(rol)
